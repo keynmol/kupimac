c6133d5956783ef483ab5031b8fa5ee31f175c47
Artem Pyanykh <a.pyanych@gmail.com>
2013-07-13 23:17:18 +0400

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kupimac::Application.config.secret_key_base = '3f428c3ac19c9b7ed0aee2c401259ae190ed8d1d3d1f6a2edd90b0a67f964817811e2517f2098bc75a4d2573dae6a37c30f04a2dac3862fc27fdc98563f4fb2a'
